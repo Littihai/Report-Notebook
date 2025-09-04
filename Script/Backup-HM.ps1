ff668546c73ec00d88fd8e02d3dabd6824b7e98d
Lttichai Yorach <04726ly@tse.com>
2025-09-04 12:09:44 +0700

    script

---
+++ Script/Backup-HM.ps1
@@ -0,0 +1,4 @@
+﻿move-item "D:\Liitichai_Yorach\Script\Report\Report-HM1.xlsm" ("D:\Liitichai_Yorach\Script\Backup\Report-HM1_{0:yyyyMMdd}.xlsm" -f (get-date -uformat "%Y%m%d_%H%M"))
+
+
+
---
+++ Script/Backup-TG.ps1
@@ -0,0 +1,4 @@
+﻿move-item "D:\Liitichai_Yorach\Script\Report\Report-TG1.xlsm" ("D:\Liitichai_Yorach\Script\Backup\Report-TG1_{0:yyyyMMdd}.xlsm" -f (get-date -uformat "%Y%m%d_%H%M"))
+
+
+
